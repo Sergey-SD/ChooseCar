079d27b3885c553fb1bc0ff979108a30d3f9b490
Sergey <stanislavsky.s@gmail.com>
2015-03-29 23:41:40 +0300

    Add comments

--- static/js/app.js
+++ static/js/app.js
@@ -7,38 +7,38 @@ $(function () {
 
 
     function carModel() {
-    	var carModels = allCars[$("#car_brand").val()];
+    	var carModels = allCars[$("#car_brand").val()];    // Get car's model's list from first select  
     	$("#car_model").empty();
-    	for (var i in carModels) {
+    	for (var i in carModels) {                         // Push car's model's list to second select
     		$("#car_model").append('<option>' + carModels[i] + '</option>');
     	};
     };
 	$("#car_brand").change(carModel);
-    carModel();
+    carModel();                                            // Initialization second select
     
     function outputCar() {
     	var carBrand = $("#car_brand").val();
     	var carModels = $("#car_model").val();
-    	var reg1 = /^\d+(,|\.)?\d*$/;
-    	var reg2 = /^0(?=\d)/;
-    	var reg3 = /(,|\.)$/;
+    	var reg1 = /^\d+(,|\.)?\d*$/;                      // For checking value dintance in order to it don't contain letters, spaces etc. 
+    	var reg2 = /^0(?=\d)/;                             // For checking numbers like 02745...
+    	var reg3 = /(,|\.)$/;                              // For checking numbere which ended on . or ,
     	var dist = $("#distance").val();
     	if ((reg1.test(dist)) && (!reg2.test(dist)) && (!reg3.test(dist))) {
-    		$(".container").empty();
+    		$(".container").empty();                                                                     // Clear page
     		$(".container").append('<div class="output-car">Выбран производитель ' + 
     			carBrand + ', модель ' + carModels + ', с пробегом ' + dist + ' км</div>');
     	}
     	else {
-    		$("#alarm").remove();
+    		$("#alarm").remove();                                                                        // If number is wrong we ask to change it
     		$("#output").append('<div id="alarm" class="alarm">Введите верное значение пробега</div>');
     	}; 
     }
 
-    $("button").click(outputCar);
-    $("form").keydown(function(eventObject) {
-        if (eventObject.which === 13) {
+    $("button").click(outputCar);                          // Button event
+    $("form").keydown(function(eventObject) {              
+        if (eventObject.which === 13) {                    // Check "Enter" button
             outputCar();
-            return false;
+            return false;                                  // Forbid to send data to server 
         };
     });
 });
